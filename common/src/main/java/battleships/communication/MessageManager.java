fe7f1927ea096325faf0c6f15611033f912d63ce
igasm <iga.smarz@gmail.com>
2017-12-11 21:38:48 +0100

    cleaning up

--- common/src/main/java/battleships/communication/MessageManager.java
+++
@@ -1,9 +0,0 @@
-package battleships.communication;
-
-import java.util.List;
-
-public interface MessageManager {
-
-    String toMessage(Messagable messegable);
-    List<Messagable> fromMessage(String message);
-}

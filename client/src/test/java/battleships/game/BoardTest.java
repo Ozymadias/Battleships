ac260b585a783e356f4b69155fb291c8991a42f8
iga <iga.smarz@gmail.com>
2017-12-21 11:00:30 +0100

    changes as Emilia requested in review of pull request Ships generation #41

--- client/src/test/java/battleships/game/BoardTest.java
+++ client/src/test/java/battleships/game/BoardTest.java
@@ -1,9 +1,6 @@
 package battleships.game;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,19 +24,6 @@ public class BoardTest {
         Board sequencesGenerator = Board.build();
         //when - then
         assertThat(sequencesGenerator.getFields().size()).isEqualTo(100);
-    }
-
-    @DataProvider
-    public Object[] randomPollOfTenFieldsPositions(){
-        return new Random().ints(10, 0, 99).boxed().toArray();
-    }
-
-    @Test(dataProvider = "randomPollOfTenFieldsPositions")
-    public void whenBuildingBoardWithCleanFields_eachFieldShouldBeEmpty(Integer fieldPosition){
-        //given
-        Board board = Board.build();
-        //when - then
-        assertThat(board.getFields().get(fieldPosition).isEmpty()).isTrue();
     }
 
 }

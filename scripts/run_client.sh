d96dffdd352b29e49e908d04ddfa0e68c6c27f3d
igasm <31968408+igasm@users.noreply.github.com>
2018-01-22 16:22:15 +0100

    Choosing the language depends on configuration variable. (#133)

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -jar ../client/target/client*.jar
+#usage: ./run_client <optional: language_shortcut>
+#possible languages shortcuts: PL, EN
+java -jar ../client/target/client*.jar $1

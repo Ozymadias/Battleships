35871c4887c6e5bf4cf2012c3913ec3bc181bdf3
igasm <31968408+igasm@users.noreply.github.com>
2018-01-25 12:46:21 +0100

    port validation on server (#143)
    * validation of server port
    * Extracting running server from main method.
    * test added, checkstyle issues resolved

---
+++ server/src/test/java/battleships/ServerAppTest.java
@@ -0,0 +1,45 @@
+package battleships;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.spi.LoggingEvent;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ServerAppTest {
+
+  private Appender mockAppender = mock(Appender.class);
+  @Captor
+  private ArgumentCaptor<LoggingEvent> captorLoggingEvent = ArgumentCaptor.forClass(LoggingEvent.class);
+
+  @BeforeClass
+  public void setUp(){
+    LogManager.getRootLogger().addAppender(mockAppender);
+  }
+
+  @AfterClass
+  public void tearDown(){
+    LogManager.getRootLogger().removeAppender(mockAppender);
+  }
+
+  @Test
+  public void whenServerAppRunWithWrongPortNumber_expectedLogWithInformationAboutInvalidPortNumber() throws UnsupportedEncodingException {
+    System.setProperty("port", "xxx");
+    ServerApp.main(null);
+
+    verify(mockAppender).doAppend(captorLoggingEvent.capture());
+    LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+
+    assertThat(loggingEvent.getRenderedMessage()).containsPattern("is not valid port number");
+  }
+
+}
